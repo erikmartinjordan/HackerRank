2f6adf43002b6642fc3183229dd855c72e6c625e
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-07-03 18:10:36 +0200

    Recoded, but I still didn't come with the right approach...

--- Recursion and Backtracking/CrosswordPuzzle.js
+++ Recursion and Backtracking/CrosswordPuzzle.js
@@ -3,45 +3,51 @@ function crosswordPuzzle(crossword, hints) {
     // Array of words
     let words = hints.split(';');
 
-    // Horizontal and vertical words to fit
-    let horizontalWords = [];
-    let verticalWords   = [];
+    // The tricky point of the problem is that you need to realize that 
+    // there is only one way to write the words.
+    // Every time we place a word wrong, we need to begin again to iterate 
+    const shuffleWords = () => {
 
-    // Defining start and end 
-    let startH = null;
-    let endH   = null;
-    let startV = null;
-    let endV   = null;
+        // Creating a copy of the crossword
+        let copy = [...crossword];
 
-    // Looping through crossword to find horizontal and vertical words
-    for(let i = 0; i < 10; i ++){
-        for(let j = 0; j < 10; j ++){
+        // Create a copy of the original array to be randomized
+        let shuffle = [...words];
 
-            // Getting horizontal words
-            if(crossword[i][j] === '-' && !startH)        startH = [i, j];
-            if(crossword[i][j] === '+' && startH)         endH   = [i, j];
-            if(startH && endH && endH[1] - startH[1] > 1) horizontalWords.push([startH, endH]);
+        // Defining function returning random value from i to N
+        const getRandomValue = (i, N) => Math.floor(Math.random() * (N - i) + i);
 
-            // Getting vertical words
-            if(crossword[j][i] === '-' && !startV)        startV = [j, i];
-            if(crossword[j][i] === '+' && startV)         endV   = [j, i];
-            if(startV && endV && endV[0] - startV[0] > 1) verticalWords.push([startV, endV]);
+        // Shuffle a pair of two elements at random position j
+        shuffle.forEach( (elem, i, arr, j = getRandomValue(i, arr.length)) => [arr[i], arr[j]] = [arr[j], arr[i]] );
 
-            // Resetting variables
-            if(startH && endH) startH = endH = null;
-            if(startV && endV) startV = endV = null;
+        // Reading words one by one
+        while(shuffle.length > 0){
 
-        }
+            // Getting first word
+            let word = shuffle.shift();
 
-        // Pushing if arrived to the end
-        if(startH) horizontalWords.push([[startH], [i, 9]]);
-        if(startV) verticalWords.push([startV,[9, i]]);
-        
-        // Resetting variables
-        startH = null;
-        startV = null;
+            // Iterating the crossword horizontally
+            for(let i = 0; i < 10; i ++){
+                for(let j = 0; j < 10; j ++){
+                    // Trying to write the word horizontally
+                    if(copy[i][j] === '-' && word.length > 0) 
+                        [copy[i][j], word] = [word[0], word.substr(1)];
+                    // The word is shorter than the space to place it
+                    if(copy[i][j] === '-' && word.length === 0)
+                        shuffleWords();
+                }
+                // The word is longer than the space to place it
+                if(word.length > 0) 
+                    shuffleWords();
+                else
+                    break;
+            }
 
-    }
+        }
+               
+    }
 
+    // Starting the function
+    shuffleWords();
 
 }
