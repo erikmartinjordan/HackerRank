1f1df2415824b86c96a1d63974e0ca5efbf50ac3
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-07-04 14:20:01 +0200

    Pass test cases but I need to beautify it, because it looks ugly

--- Recursion and Backtracking/CrosswordPuzzle.js
+++ Recursion and Backtracking/CrosswordPuzzle.js
@@ -1,109 +1,115 @@
 function crosswordPuzzle(crossword, hints) {
-    
-    // Array of words
-    let words = hints.split(';');
-    
+
+    // Array of words
+    let words = hints.split(';');
+
+    // Result
+    let res = [];
+
     // Every time we place a word wrong, function restarts
     const shuffleWords = () => {
-        
+
         // Creating a copy of the crossword
         let copy = [...crossword];
-        
+
         // Creating a copy of the original array to be randomized
         let shuffle = [...words];
-        
+
         // Defining function returning random value from i to N
         const getRandomValue = (i, N) => Math.floor(Math.random() * (N - i) + i);
-        
+
         // Declaring function to see if word fits
         const wordFits = (string1, string2) => [...string1].reduce( (acc, elem, i) => acc = acc && (string1[i] === '-' || string1[i] === string2[i]), true);
-        
+
         // Shuffling a pair of two elements at random position j
         shuffle.forEach( (elem, i, arr, j = getRandomValue(i, arr.length)) => [arr[i], arr[j]] = [arr[j], arr[i]] );
-        
+
         // Reading words one by one
         while(shuffle.length > 0){
-            
+
             // Getting first word
             let word = shuffle.shift();
-            
+
             // Word in dashes
             let wordInDashes = '-'.repeat(word.length);
-            
+
             // Determine if the word was written
             let written = false;
-            
+
             // Iterating the crossword horizontally
-            for(let i = 0; i < 10; i ++){
-                
-                // Getting the dashes of the line
-                let lineDashes = copy[i].split('+').join('');
-                
-                // Comparing if the word fits in the dashes
-                if(lineDashes.length === wordInDashes.length && lineDashes.includes('-') && wordFits(lineDashes, word)){
+            for(let i = 0; i < 10 && !written; i ++){
+
+                // Getting the potential words in a line
+                let potentialWords = copy[i].split('+').filter(elem => elem !== '');
+
+                // Iterating through potential words
+                potentialWords.forEach(potentialWord => {
+
+                    // Comparing if the word fits in the dashes
+                    if(potentialWord.length === wordInDashes.length && potentialWord.includes('-') && wordFits(potentialWord, word)){
                     
-                    // Replacing the line
-                    copy[i] = copy[i].replace(lineDashes, word);
-                    
-                    // Word is written
-                    written = true;
-                    
-                    // Going out of the loop
-                    break;
-                    
-                }
-                
+                        // Replacing the line
+                        copy[i] = copy[i].replace(potentialWord, word);
+
+                        // Word is written
+                        written = true;
+
+                    }
+
+                });
+
             }
-            
+
             // If the word is not written horizontally, let's try it vertically
             if(!written){
                 
                 // Transpose the crossword
                 let trans = copy.map((string, i) => [...string].reduce( (acc, elem, j) => acc += copy[j][i], ''));
-                
+
                 // Iterating the crossword horizontally
-                for(let i = 0; i < 10; i ++){
-                    
-                    // Getting the dashes of the line
-                    let lineDashes = trans[i].split('+').join('');
-                    
-                    // Comparing if the word fits in the dashes
-                    if(lineDashes.length === wordInDashes.length && lineDashes.includes('-') && wordFits(lineDashes, word)){
+                for(let i = 0; i < 10 && !written; i ++){
+
+                    // Getting the potential words in a line
+                    let potentialWords = trans[i].split('+').filter(elem => elem !== '');
+
+                    // Iterating through potential words
+                    potentialWords.forEach(potentialWord => {
+
+                        // Comparing if the word fits in the dashes
+                        if(potentialWord.length === wordInDashes.length && potentialWord.includes('-') && wordFits(potentialWord, word)){
                         
-                        // Replacing the line
-                        trans[i] = trans[i].replace(lineDashes, word);
-                        
-                        // Word is written
-                        written = true;
-                        
-                        // Transpose again
-                        copy = trans.map((string, i) => [...string].reduce( (acc, elem, j) => acc += trans[j][i], ''));
-                        
-                        // Going out of the loop
-                        break;
-                        
-                    }
-                    
+                            // Replacing the line
+                            trans[i] = trans[i].replace(potentialWord, word);
+
+                            // Word is written
+                            written = true;
+
+                            // Transpose again
+                            copy = trans.map((string, i) => [...string].reduce( (acc, elem, j) => acc += trans[j][i], ''));
+
+                        }
+
+                    });
+
                 }
-                
+
             }
-            
-            console.log(copy);
-            
+
             // If still not written, shuffle words
             if(!written)
                 shuffleWords();
+            // Otherwise save result
+            else
+                res = [...copy];
             
         }
-        
-        return copy;
-       
+
     }
-    
+
     // Starting the function
-    let res = shuffleWords();
-    
+    shuffleWords();
+
     // Returning result
     return res;
-    
+
 }
