a6a35d6b4f40d9b222c723e35d60bb712b9dd521
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-07-21 10:23:18 +0200

    Problem solved

--- Miscellaneous/MaximumXor.js
+++ Miscellaneous/MaximumXor.js
@@ -1,64 +1,77 @@
 function maxXor(arr, queries) {
 
-    // Result is an array of integers
+    // Declaring result as an array of integers
     let res = [];
-
+    
     // Creating the trie
     let trie = {};
-
-    // arr = [5, 1, 4, 3];
-    // arr = [101, 001, 100, 011];
-
-    // queries = [2, 0];
-    // queries = [010, 000];
-
-    // inverse = [101, 111];
-    /*
-
-           root
-          /    \
-         1      0
-        /       / \
-       10      00  01
-      /  \       \   \
-     100 101    001 011
-
+    
+    // Declaring empty strings  
+    let num = '';
+    
     // Iterating through array
     for(let i = 0; i < arr.length; i ++){
-
+        
+        // Defining empty 32-bits array
+        let empty = Array(32).fill(0).map(elem => elem).join('');
+        
         // Transforming number to binary
-        let binary = arr[i].toString(2);
-
+        let binary = (empty + arr[i].toString(2)).slice(-32);
+        
         // Iterating through binary number
         for(let j = 0; j < binary.length; j ++){
-
+            
+            // Adding binary number to string
             num += binary[j];
-
-            if(num !=== binary){
-                trie[num].left  = num + 0;
-                trie[num].right = num + 1; 
+                
+            // Defining empty trie
+            if(!trie[num]) trie[num] = {};
+            
+            // Creating childs 
+            if(num !== binary){
+                
+                if(binary[j + 1] === '0') trie[num].left  = num + 0;
+                if(binary[j + 1] === '1') trie[num].right = num + 1;
+                
             }
-            else{
-                trie[num].included = true;
-            }
-
+            
         }
-
+        
+        num = '';
+        
     }
-    */
     
     // Getting the max xor in queries and finding it in arr
     for(let i = 0; i < queries.length; i ++){
-
-        // Transforming the number into a binary
-        let binary = queries[i].toString(2);
-
-        // The maximum value would be the binary inverse
-        let binaryInverse = [...binary].map(num => num === '0' ? 1 : 0).join('');
-
-        // Getting max binary
-        let max = trie[binaryInverse];
-
+        
+        // Defining empty 32-bits array
+        let empty = Array(32).fill(0).map(elem => elem).join('');
+        
+        // Transforming number to binary
+        let binary = (empty + queries[i].toString(2)).slice(-32);
+        
+        // Iterating through binary number
+        for(let j = 0; j < binary.length; j ++){
+            
+            let current = binary[j];
+            let inverse = binary[j] === '1' ? '0' : '1';
+            
+            if(trie[num + inverse]) num += inverse;
+            else                    num += current;
+            
+        }
+        
+        // Getting maxXor between binary and num
+        let max = parseInt(binary, 2) ^ parseInt(num, 2);
+        
+        // Pushing result
+        res.push(max);
+        
+        // Resetting variable
+        num = '';
+        
     }
-
+    
+    return res;
+    
 }
